6f88f00b8e8973c933350065ea15c6ae514ff036
Francis KAGO <franciskago78@gmail.com>
2026-07-11 20:09:21 +0100

    chore: retirer ligne vide superflue dans layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
       lang="fr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
